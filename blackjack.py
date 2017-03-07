8b4b50e91c925c09ba1d1f0231b12096570b9078
John Doe <hashtag@fakeo.com>
2017-03-07 08:12:26 -0500

    updated from monday

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import random
-
-
-class deck:
-    def __init__(self, number_of_decks=1):
-        self.number_of_decks = number_of_decks
-        self.quantity = 52 * number_of_decks
-
+import sys
 
 class card:
     def __init__(self):
         self.suit = random.choice(suit)
         self.rank = random.choice(rank)
         self.card = deck[self.suit][self.rank]
+        try:
+            self.point = int(self.rank)
+        except Exception:
+            if self.rank == "Ace":
+                self.point = 1
+            else:
+                self.point = 10
+
 
 class hand:
     def __init__(self):
@@ -21,8 +23,18 @@ class hand:
     def add_card(self, card):
         self.handle.append(card)
 
-    def score():
-        pass
+    def validate(self, check, quant):
+        check = card
+        neo = card()
+        while check in self.handle and self.handle.count(z) == quant:
+            neo = card()
+        return neo
+
+    def score(self):
+        tally = 0
+        for x in self.handle:
+            tally += x.point
+        return tally
 
     def __str__(self):
         show_me = []
@@ -33,9 +45,8 @@ class hand:
 
 
 suit = ["Spades", "Hearts", "Diamonds", "Clubs"]
-rank = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-        "Nine", "Ten", "Jack", "Queen", "King"]
-
+rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", 
+        "9", "10", "Jack", "Queen", "King"]
 
 spade = {rank[0]: 0x1F0A1, rank[1]: 0x1F0A2, rank[2]: 0x1F0A3, rank[3]: 0x1F0A4,
          rank[4]: 0x1F0A5, rank[5]: 0x1F0A6, rank[6]: 0x1F0A7, rank[7]: 0x1F0A8,
@@ -60,24 +71,44 @@ clubs = {rank[0]: 0x1F0D1, rank[1]: 0x1F0D2, rank[2]: 0x1F0D3, rank[3]: 0x1F0D4,
 deck = {"Spades": spade, "Hearts": heart, "Diamonds": diamo, "Clubs": clubs}
 
 def main():
+    please = ["Y", "y", "yes", "YES", "Yes"]
+    thanks = ["N", "n", "no", "NO", "No", "Stay", "STAY", "stay"]
+
+    while True:
+        try:
+            number_decks = int(input("Enter number of decks (1 - 7): "))
+        except Exception:
+            print("I'm sorry, Dave...  I can't allow you to do that.")
+            continue
+        if number_decks > 0 and number_decks < 8:
+            break
 
     alpha = hand()
     x = card()
     y = card()
+    while y == x and number_decks == 1:
+        y = card()
     alpha.add_card(x)
     alpha.add_card(y)
     print(alpha)
 
-    while True:
+    while alpha.score() < 22:
         response = input("Hit? ")
-        if response is 'Y':
+        if response in please:
             z = card()
+            alpha.validate(z, number_decks)
             alpha.add_card(z)
             print(alpha)
+        elif response in thanks:
+            winrar = alpha.score()
+            print(winrar)
+            print("decent...")
+            sys.exit()
         else:
             print("RUHROH")
-            break
+            sys.exit()
 
+    print("BUUUUSTED!!!")
 
 if __name__ == '__main__':
     main()
